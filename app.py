f1446aa4d18a8d9d4ee9770eccc18d79c0da5249
cwq7nt6sn5-web <cwq7nt6sn5@privaterelay.appleid.com>
2026-04-20 17:42:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ st.write({"Name": "Alice", "Age": 30})
 st.write(['list1', 'list2'])
 
 st.write("**Bold Text** and *Italic Text*")
+
+st.header("Section 1: Introduction")
+
+age = st.number_input("Enter your age:",
+                      min_value=0,
+                      max_value=120,
+                      value=25)
+st.write(f"Your age is {age}")
